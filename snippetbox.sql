7598cd51cd71197d197f60ce0eef9871815bf450
OnkelPony <zkurvenej@outlook.com>
2022-05-15 16:26:34 +0200

    Executing SQL statements.

--- snippetbox.sql
+++ snippetbox.sql
@@ -30,7 +30,7 @@ CREATE TABLE `snippets` (
   `expires` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `idx_snippets_created` (`created`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,8 @@ LOCK TABLES `snippets` WRITE;
 INSERT INTO `snippets` VALUES
 (1,'An old silent pond','An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō','2022-05-14 17:42:55','2023-05-14 17:42:55'),
 (2,'Over the wintry forest','Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki','2022-05-14 17:43:11','2023-05-14 17:43:11'),
-(3,'First autumn morning','First autumn morning\nthe mirror I stare into\nshows my father\'s face.\n\n– Murakami Kijo','2022-05-14 17:43:32','2022-05-21 17:43:32');
+(3,'First autumn morning','First autumn morning\nthe mirror I stare into\nshows my father\'s face.\n\n– Murakami Kijo','2022-05-14 17:43:32','2022-05-21 17:43:32'),
+(4,'O snail','O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa','2022-05-15 12:58:55','2022-05-22 12:58:55');
 /*!40000 ALTER TABLE `snippets` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,4 +56,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-14 19:56:14
+-- Dump completed on 2022-05-15 16:17:04
