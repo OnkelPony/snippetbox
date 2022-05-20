f663a4c4fa1dcc68dbd30c5f5792e2c35f13fc49
Jiří Martínek <zwo@centrum.cz>
2022-05-20 12:52:48 +0200

    Custom template functions.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,12 +4,21 @@ import (
 	"github.com/OnkelPony/snippetbox/internal/models"
 	"html/template"
 	"path/filepath"
+	"time"
 )
 
 type templateData struct {
 	CurrentYear int
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("2 Jan 2006 15:04:05")
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -22,7 +31,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.gohtml")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.gohtml")
 		if err != nil {
 			return nil, err
 		}
