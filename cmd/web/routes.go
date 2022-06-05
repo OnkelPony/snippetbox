c7ee12551ddaa18afa253cf3709095ccce122c4b
OnkelPony <zkurvenej@outlook.com>
2022-06-05 12:40:51 +0200

    Using embedded files.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/OnkelPony/snippetbox/ui"
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 	"net/http"
@@ -13,8 +14,8 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/OnkelPony/snippetbox/internal/models"
+	"github.com/OnkelPony/snippetbox/ui"
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 )
@@ -27,29 +29,21 @@ func humanDate(t time.Time) string {
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
-	pages, err := filepath.Glob("./ui/html/pages/*.gohtml")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.gohtml")
 	if err != nil {
 		return nil, err
 	}
-
 	for _, page := range pages {
 		name := filepath.Base(page)
-
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.gohtml")
+		patterns := []string{
+			"html/base.gohtml",
+			"html/partials/*.gohtml",
+			page,
+		}
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
-
-		ts, err = ts.ParseGlob("./ui/html/partials/*.gohtml")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
-
 		cache[name] = ts
 	}
 	return cache, nil
