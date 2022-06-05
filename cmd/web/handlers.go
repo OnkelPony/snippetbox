452dd1ba8ce4c12dc1d522031dc9a06e7eb7f215
OnkelPony <zkurvenej@outlook.com>
2022-06-05 15:54:49 +0200

    Using generics.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -81,7 +81,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
 	form.CheckField(validator.MaxChars(form.Title, 108), "title", "This field cannot be longer than 108 chars")
 	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
-	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must be equal 1, 7 or 365")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must be equal 1, 7 or 365")
 
 	if !form.Valid() {
 		data := app.newTemplateData(r)
